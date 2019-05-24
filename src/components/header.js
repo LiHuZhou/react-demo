5d20f43121978622c4546d47495f809502e2e6c3
lihu.zhou <lihu.zhou@wm-holding.com.cn>
2019-05-24 16:03:47 +0800

    'demo'

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <h1> 这是一个头部 </h1>
+    }
+}
